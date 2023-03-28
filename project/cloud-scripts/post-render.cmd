5cafdef1ff24495ab221b64320139f0cf68a6c67
Matthew Ragan <raganmd@gmail.com>
2023-03-28 13:28:10 -0700

    Syncs Logs from Cloud Render to S3

--- project/cloud-scripts/post-render.cmd
+++ project/cloud-scripts/post-render.cmd
@@ -4,3 +4,4 @@ aws s3 sync _outputs/data s3://cibic21-viz-website/data
 aws s3 sync _outputs/photos s3://cibic21-viz-website/photos
 aws s3 sync _outputs/renderings s3://cibic21-viz-website/renderings
 aws s3 sync _exports/pickle_jar s3://cibic21-viz-website/cache
+aws s3 sync _public/log s3://cibic21-viz-website/log
